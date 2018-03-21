21d077f0fe10376a403eb6230c0e21abafc4938a
brandonlind <lindb@vcu.edu>
2018-03-21 17:16:51 -0400

    imports.R

--- imports.R
+++ imports.R
@@ -1,8 +1,12 @@
-libs = c('tidyr','hash','SDMTools','dplyr')
+options(stringsAsFactors = FALSE)
+options(digits = 20)
 
+libs = c('tidyr','hash','SDMTools','dplyr')
 len = length
 uni = unique
 sorted = sort
+cd = setwd
+
 
 endsWith = function(x, suffix) {
   if (!is.character(x) || !is.character(suffix))
@@ -43,6 +47,74 @@ install = function(x){
 	install.packages(as.character(x),dependencies=TRUE, repos='http://cran.rstudio.com/',quiet=TRUE)
 	library(x,character.only=TRUE)
 	cat(sprintf("Installed and loaded %s library", x))
+}
+
+findsep <- function(f){
+	if (endsWith(f,"/")){
+		sep = ""
+	} else {
+		sep = "/"
+	}
+	return(sep)
+}
+
+fs <- function(dir){
+	files = list.files(dir)
+
+	sep = findsep(dir)
+	
+	lst = c()
+	for (f in files){
+		lst = c(lst,paste(dir,f,sep=sep))
+	}
+
+	return(lst)
+}
+
+fname <- function(dir=NULL,f){
+	if (class(dir)=='NULL'){
+		dir = getwd()
+	}
+	sep = findsep(dir)
+
+	return(paste(dir,f,sep=sep))
+}
+
+rds = function(DIR=getwd(),erase=FALSE){
+	if (erase == TRUE){
+		cat(sprintf("Are you sure you want to delete all files in %s\n",DIR))
+		rdsfiles = list.files(DIR,pattern='.RDS')
+		allfiles = list.files(DIR)
+		cat(sprintf("There are %s RDS files out of %s total files",len(rdsfiles),len(allfiles)))
+		cat(sprintf("Planning on saving %s RDS files from environment",len(ls(envir=.GlobalEnv))))
+		choice <- menu(c("yes","no"))
+		if (choice == 1){
+			newdir = fname('~/.Trash',gsub(" ","_",Sys.time())) # so we don't overwrite in trash which could be bad
+			newdir = gsub(":","-",newdir)
+			if (!file.exists(newdir)){
+				dir.create(newdir)
+			}
+			star = gsub(" ","\\\\ ",fname(DIR,'*RDS'))
+			system(sprintf("mv %s %s",star,newdir))
+			saveRDS(DIR,fname(newdir,'0source_DIR.RDS'))
+		}
+	}
+	for (obj in ls(envir=.GlobalEnv)){
+    	saveRDS(get(obj),fname(DIR,sprintf("%s.RDS",obj)))
+	}
+}
+
+
+getrds = function(DIR=getwd()){
+	for (f in list.files(DIR)){
+	    if (endsWith(f,'RDS'))
+	    {
+	        cat(sprintf ("loading %s file\n",f))
+	        F = fname(DIR,f)
+	        obj = strsplit(f,split='.RDS')[[1]]
+	        assign(obj,readRDS(F),envir=.GlobalEnv)    
+	    }
+	}
 }
 
 #for (lib in libs){
